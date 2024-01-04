14aff1c9771d36b612c3588cf14ffc953947af5e
Your Name <you@example.com>
2024-01-04 11:36:25 +0100

    tt

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var ghpages = require('gh-pages');
+// var ghpages = require('gh-pages');
 
-ghpages.publish('build', function(err) {});
+// ghpages.publish('build', function(err) {});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
